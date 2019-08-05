29ac1f39e571ee8d2eea105c7f499463738f936f
Juliette Abeyta <jabeyta@fluentstream.com>
2019-08-04 20:41:30 -0600

    📰 Finish styling blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,6 +11,18 @@ import blogheader from "../images/blogheader.jpg"
 import jsaIcon from "../images/jsa-icon.png"
 
 class Blog extends React.Component {
+  onMouseEnter(e) {
+    const index = e.currentTarget.id.split('-')[1];
+    const blogEntryImageID = `blog-entry-image-${index}`;
+    let blogEntryImage = document.getElementById(blogEntryImageID);
+    blogEntryImage.classList.add('blog-entry-image-zoom');
+  }
+  onMouseLeave(e) {
+    const index = e.currentTarget.id.split('-')[1];
+    const blogEntryImageID = `blog-entry-image-${index}`;
+    let blogEntryImage = document.getElementById(blogEntryImageID);
+    blogEntryImage.classList.remove('blog-entry-image-zoom');
+  }
   render() {
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     let blogpostlinks = [];
@@ -21,8 +33,14 @@ class Blog extends React.Component {
           key={index}
           to={`/${post.node.slug}`}
           state={{ post: post.node.body.childMarkdownRemark.html, description: post.node.description.description, title: post.node.title, image: post.node.heroImage }}
-          className="blog-entry">
-          <img src={post.node.heroImage.fixed.src} alt={post.node.heroImage.description} />
+          className="blog-entry"
+          id={`entry-${index}`}
+          onMouseEnter={(e) => this.onMouseEnter(e)}
+          onMouseLeave={(e) => this.onMouseLeave(e)}
+        >
+          <div className="blog-entry-image-container">
+            <img src={post.node.heroImage.fixed.src} alt={post.node.heroImage.description} className="blog-entry-image" id={`blog-entry-image-${index}`} />
+          </div>
           <div className="blog-entry-title"><b>{post.node.title}</b><p>{post.node.description.description}</p></div>
         </Link>);
     });
@@ -38,7 +56,7 @@ class Blog extends React.Component {
             <img src={blogheader} alt="Green plant with a dark background" className="blog-image" />
           </div>
           <div className="blog-body">
-            <b>Hello!</b>
+            <h1>Hello!</h1>
             <p>Welcome to my mini blog series!
             Each week I will relay the information I have obtained
             on various topics. This is an attempt at furthering my
@@ -56,7 +74,7 @@ class Blog extends React.Component {
   }
 }
 
-export default Blog
+export default Blog;
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
--- src/pages/blogpost.js
+++ src/pages/blogpost.js
@@ -1,24 +1,28 @@
 import React from "react"
-
+import { Link } from "gatsby";
 import SEO from "../components/seo"
+
 
 const BlogPost = ({ location }) => {
 
   const post = location.state.post;
   const imageobj = location.state.image;
+  const title = location.state.title;
   let img = [];
   if (imageobj && imageobj.fixed && imageobj.fixed.src && imageobj.description) {
-    img.push(<img src={imageobj.fixed.src} alt={imageobj.description} className="blog-image" />)
+    img.push(<img src={imageobj.fixed.src} alt={imageobj.description} className="blog-post blog-image" />)
   }
   return (
-    <>
+    <div className="blog">
       <SEO title={location.state.description} />
-      <div>
-        <h1>{location.state.title}</h1>
+      <div className="blog-header">
+        <div className="blog-nav">
+          <Link to="/blog">Back</Link>
+        </div>
         {img}
       </div>
-      <div className="blog" dangerouslySetInnerHTML={{ __html: `${post}` }}>
+      <div className="blog-body" dangerouslySetInnerHTML={{ __html: `<h1>${title}</h1><hr>${post}` }}>
       </div>
-    </>)
+    </div>)
 }
 export default BlogPost
