2977b97101104af32b26305226c984fc16744469
Juliette Abeyta <jabeyta@fluentstream.com>
2019-09-16 21:18:38 -0600

    Fix tags for projects

--- src/pages/work.js
+++ src/pages/work.js
@@ -7,14 +7,14 @@ const SecondPage = () => (
       title="Initiative"
       date="05.19-present"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-      tags={["Javascript"]}
+      tags={["React", "Leadership"]}
     />
     <Project
       url="https://fibonacci.juliette.dev"
       title="Fibonacci Generator"
       date="06.19"
       description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-      tags={["React", "Leadership"]}
+      tags={["Javascript"]}
     />
     <Project
       url="https://halloween.juliette.dev"
