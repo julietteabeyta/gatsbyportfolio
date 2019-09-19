69b0cc00143128143e2413bf6ec416fc6a54a5d2
Juliette Abeyta <jabeyta@fluentstream.com>
2019-09-18 19:18:49 -0600

    Style header and move styles out of template for layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,14 +11,7 @@ import codepen from "../images/codepen.svg"
 
 const Layout = ({ children }) => {
   return (
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
+    <div className="content-container">
       <div className="nav">
         <Link id="home-nav" to="/">
           <img id="app-icon" src={jsaIcon} alt="Juliette Icon" />
