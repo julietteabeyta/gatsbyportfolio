7d6e01e9961caf731008bd8be3c1ecdb319d7068
Juliette Abeyta <jabeyta@fluentstream.com>
2019-09-25 21:09:52 -0600

    Remove unnecessary comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 const transitionDelay = 250
 
 exports.shouldUpdateScroll = ({
