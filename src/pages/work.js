1e1a61abafabb3faf309ff878a5fe5314aacf210
Juliette Abeyta <juneauwh0a@gmail.com>
2019-09-26 09:51:53 -0600

    Add SpaceX project to work section 🚀

--- src/pages/work.js
+++ src/pages/work.js
@@ -10,9 +10,16 @@ const SecondPage = () => (
       tags={["React", "Leadership"]}
     />
     <Project
+      url="https://spacex.juliette.dev"
+      title="SpaceX Launches"
+      date="09.19"
+      description="This is a single page React application that allows users to search the SpaceX GraphQL server for upcoming and past SpaceX launches by mission name, rocket name, or launch year. Each mission matching the search returns with this data, and a launch video link if found."
+      tags={["React", "GraphQL"]}
+    />
+    <Project
       url="https://fibonacci.juliette.dev"
       title="Fibonacci Generator"
-      date="06.19"
+      date="07.19"
       description="This app does as the name implies; given a user-supplied input of desired sequence values, a Fibonacci sequence is returned. I knew very little about the sequence before researching it, and it only seemed right to build an application to accompany my findings."
       tags={["Javascript"]}
     />
@@ -21,7 +28,7 @@ const SecondPage = () => (
       title="Halloween Countdown"
       date="04.19"
       description="This aptly name countdown application is my most recent employment of Three.js. I also had the challenge of managing dates to calculate the time until Halloween regardless of year. It is never too early to check how long until the next All Hallows Eve. Happy haunting!"
-      tags={["React", "Three.js", "Javascript"]}
+      tags={["React", "Three.js"]}
     />
     <span id="link-new-window" hidden>
       Opens in a new window
