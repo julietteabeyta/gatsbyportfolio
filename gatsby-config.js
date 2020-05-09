91db62bac2ee12e756815db06209e6778cf94494
Juliette Abeyta <juliette.abeyta@evgo.com>
2020-05-09 14:32:14 -0600

    Add in soehne font and tidy up some styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/jsaWhiteLogo.png`, // This path is relative to the root of the site.
+        icon: `src/images/jsaBlackLogo.png`, // This path is relative to the root of the site.
       },
     },
     {
